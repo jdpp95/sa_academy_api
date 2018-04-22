2efa988c720c18c8c0210d678ae6161c675213cb
jdpp95 <jdporrasp@unal.edu.co>
2018-04-22 16:25:49 -0500

    Routing to students auth added

--- src/graphQLSchema.js
+++ src/graphQLSchema.js
@@ -22,9 +22,15 @@ import {
 	gradesTypeDef
 } from './grades/typeDefs';
 
+import {
+    authMutations,
+    authTypeDef
+} from './auth/typeDefs';
+
 import studentsResolvers from './students/resolvers';
 import coursesResolvers from './courses/resolvers';
 import gradesResolvers from './grades/resolvers';
+import authResolvers from './auth/resolvers';
 
 // merge the typeDefs
 const mergedTypeDefs = mergeSchemas(
@@ -32,7 +38,8 @@ const mergedTypeDefs = mergeSchemas(
 		'scalar JSON',
 		studentsTypeDef,
 		coursesTypeDef,
-		gradesTypeDef
+		gradesTypeDef,
+		authTypeDef
 	],
 	[
 		studentsQueries,
@@ -51,6 +58,9 @@ export default makeExecutableSchema({
 	typeDefs: mergedTypeDefs,
 	resolvers: merge(
 		{ JSON: GraphQLJSON }, // allows scalar JSON
-		studentsResolvers
+		studentsResolvers,
+        coursesResolvers,
+        gradesResolvers,
+        authResolvers
 	)
 });
